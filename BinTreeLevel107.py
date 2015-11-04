3e672857da8560b7089a1c9d36cc56ff51078a07
Jacy-Wang <hitwangyue@gmail.com>
2015-11-04 13:15:54 -0500

    Add 107 binary tree level order

---
+++ BinTreeLevel107.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+import Queue
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root:
+            res = []
+            q = Queue.Queue()
+            q.put_nowait((0, root))
+            level = -1
+            while not q.empty():
+                item = q.get_nowait()
+                if item[0] == level + 1:
+                    if level > -1:
+                        res.append(levelRes)
+                    levelRes = [item[1].val]
+                    level = level + 1
+                else:
+                    levelRes.append(item[1].val)
+                if item[1].left:
+                    q.put_nowait((level + 1, item[1].left))
+                if item[1].right:
+                    q.put_nowait((level + 1, item[1].right))                
+            res.append(levelRes)
+            res.reverse()
+            return res
+        return []
