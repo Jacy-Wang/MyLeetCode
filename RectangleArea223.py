1507e16a92475d0486cec194191ab711e6a6b6f7
Jacy-Wang <hitwangyue@gmail.com>
2015-11-11 13:33:15 -0500

    Add 223 rectangle area

---
+++ RectangleArea223.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        area1 = (C - A) * (D - B)
+        area2 = (G - E) * (H - F)
+        area3 = max(0, min(C, G) - max(A, E)) * max(0, min(D, H) - max(B, F))
+        return area1 + area2 - area3
