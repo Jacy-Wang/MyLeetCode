61df7f2af8812f4ce8d8154fe71e1db41712c8f6
Jacy-Wang <hitwangyue@gmail.com>
2015-11-16 22:24:24 -0500

    Update 62 unique paths

--- UniquePaths62.py
+++ UniquePaths62.py
@@ -5,11 +5,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        nums = [[0 for _ in xrange(n)] for _ in xrange(m)]
-        for i in xrange(m):
-            for j in xrange(n):
-                if i == 0 or j == 0:
-                    nums[i][j] = 1
-                elif i > 0 and j > 0:
-                    nums[i][j] = nums[i - 1][j] + nums[i][j - 1]
+        nums = [[1 for _ in xrange(n)] for _ in xrange(m)]
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                nums[i][j] = nums[i - 1][j] + nums[i][j - 1]
         return nums[m - 1][n - 1]
