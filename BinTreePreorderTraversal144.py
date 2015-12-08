d6faf330b118be57dea3ac3fa38c07c96e9d8277
Jacy-Wang <hitwangyue@gmail.com>
2015-12-08 15:48:58 -0500

    Add 144 binary tree preorder traversal

---
+++ BinTreePreorderTraversal144.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = []
+        stack = []
+        stack.append(root)
+        while stack:
+            top = stack.pop()
+            res.append(top.val)
+            if top.right:
+                stack.append(top.right)
+            if top.left:
+                stack.append(top.left)
+        return res
