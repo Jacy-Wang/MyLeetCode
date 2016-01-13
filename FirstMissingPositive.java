178e45fabac2ed52cd8bebabbae72fad49371ac8
Jacy-Wang <hitwangyue@gmail.com>
2016-01-13 01:53:00 -0500

    Add 41 first missing positive jave version

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -1,7 +1,7 @@
 public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-        	while (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
+        	while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
         		int tmp = nums[nums[i] - 1];
         		nums[nums[i] - 1] = nums[i];
         		nums[i] = tmp;
