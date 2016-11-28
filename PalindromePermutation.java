012675e4423e64e5e32a2a921b6f63a1d7a27352
Jacy-Wang <hitwangyue@gmail.com>
2016-11-28 12:41:19 -0500

    Add 266 in java.

---
+++ PalindromePermutation.java
@@ -0,0 +1,18 @@
+public PalindromePermutation {
+    public boolean canPermutePalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        int oddNum = 0;
+        for (Character c : map.keySet()) {
+            if (map.get(c) & 1 == 1)
+                oddNum++;
+        }
+        return oddNum <= 1;
+    }
+}
